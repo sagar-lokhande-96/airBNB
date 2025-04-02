f454cb1b9aeff3bd1d3a473023a2f903df472e6d
Sagar Lokhande <sagarlokhande159@gmail.com>
2025-04-02 08:50:31 +0530

    adding the edit & update route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 8000;
 import Listing from "./models/listing.js";
 import path from "path";
 import { fileURLToPath } from "url"; 
+import methodOverride from "method-override";
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -31,6 +32,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended:true})); 
+app.use(methodOverride('_method'));
 
 app.get("/", (req, res) => {
   console.log("hello i am from roote");
@@ -67,6 +69,22 @@ app.post("/listings",async (req,res)=>{
   //console.log("successfull sends!!");
   res.redirect("/listings");
 })
+
+// Edit Route 
+app.get("/listings/:id/edit",async(req,res)=>{
+  let {id} = req.params;
+  let listing = await Listing.findById(id);
+  res.render("listings/edit",{listing});
+});
+
+
+// Update Route
+app.put("/listings/:id",async(req,res)=>{
+  let {id} = req.params;
+  await Listing.findByIdAndUpdate(id, {...req.body.listing});
+  res.redirect(`/listings/${id}`);
+});
+
 
 app.listen(PORT, () => {
   console.log(`app rendered on port http://localhost:${PORT}/listings`);
