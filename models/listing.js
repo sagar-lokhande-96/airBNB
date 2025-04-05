ef338ccd7d4ec365583e5a1a75131f7740bcde9a
Sagar Lokhande <sagarlokhande159@gmail.com>
2025-04-05 14:57:44 +0530

    solve DB-error , add css into the edit page

--- models/listing.js
+++ models/listing.js
@@ -10,16 +10,12 @@ const listingSchema = new Schema({
     type: String,
   },
   image: {
-    filename: String,
-    url: {
-      type: String,
-      default:
-        "https://images.unsplash.com/photo-1568605114967-8130f3a36994",
-      set: (v) =>
-        v === ""
-          ? "https://images.unsplash.com/photo-1568605114967-8130f3a36994"
-          : v,
-    },
+    type: String,
+    default: "https://images.unsplash.com/photo-1568605114967-8130f3a36994",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1568605114967-8130f3a36994"
+        : v,
   },
   price: Number,
   location: String,
